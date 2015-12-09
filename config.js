e11bf4c40ce5e28468a963d90b34b1d4d85d4bf2
Goran Košutić <kosutic.goran@gmail.com>
2015-12-09 14:17:18 +0100

    add jquery for easier dom manipulation in tests

--- config.js
+++ config.js
@@ -16,6 +16,7 @@ System.config({
     "babel": "npm:babel-core@5.8.34",
     "babel-runtime": "npm:babel-runtime@5.8.34",
     "core-js": "npm:core-js@1.2.6",
+    "jquery": "github:components/jquery@2.1.4",
     "tuio": "npm:tuio-extended@0.1.0",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
