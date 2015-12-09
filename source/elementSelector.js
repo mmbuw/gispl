a36b72f2e8350f2e795c836c94dcccb834d89e26
Goran Košutić <kosutic.goran@gmail.com>
2015-12-09 14:18:51 +0100

    add a component to check whether a tuio point hits a dom node // not finished, probably slow, needs screen dimensions

---
+++ source/elementSelector.js
@@ -0,0 +1,51 @@
+export default function elementSelector() {
+    
+    let selectorApi = {};
+    
+    selectorApi.find = function selectorFind(params = {}) {
+        let nodes = [],
+            {x, y} = params,
+            topElement = element(document.elementFromPoint(x, y));
+        
+        while (topElement.exists()) {
+            if (topElement.containsPoint({x, y})) {
+                nodes.push(topElement.node);
+            }
+            topElement.parent();
+        }
+        return nodes;
+    };
+    
+    return selectorApi;
+}
+
+
+function element(paramNode) {
+    let elementApi = {};
+    
+    elementApi.node = paramNode;
+    
+    elementApi.exists = function elementExists() {
+        return this.node !== null;
+    };
+    
+    elementApi.parent = function elementParent() {
+        this.node = this.node.parentNode;
+    };
+    
+    elementApi.containsPoint = function elementContainsPoint(point) {
+        let elementGeometry,
+            {x, y} = point;
+        
+        if (this.node.getBoundingClientRect) {
+            elementGeometry = this.node.getBoundingClientRect();
+        }
+        return (this.node === document || (
+                    elementGeometry.left <= x &&
+                    x < elementGeometry.right &&
+                    elementGeometry.top <= y &&
+                    y < elementGeometry.bottom));
+    };
+    
+    return elementApi;
+}
---
+++ tests/unit/elementSelector.js
@@ -0,0 +1,128 @@
+import $ from 'jquery';
+import elementSelector from '../../source/elementSelector';
+
+describe('ElementSelector', () => {
+    
+    let selector,
+        appendedTestElements = [],
+        helper = {
+            appendElement: function (options = {}) {
+                let {
+                    type = 'div',
+                    parent = 'body',
+                    marginLeft = 0,
+                    marginTop = 0,
+                    position = 'static',
+                    left = 0,
+                    top = 0
+                } = options;
+                
+                let element = $(`<${type} style="
+                            width: 10px;
+                            height: 10px;
+                            margin-left: ${marginLeft}px;
+                            margin-top: ${marginTop}px;
+                            position: ${position};
+                            left: ${left}px;
+                            top: ${top}px;"></${type}>`).appendTo(parent);
+        
+                appendedTestElements.push(element);
+                
+                return element[0];
+            },
+            assertNodesExpectation: function (foundNodes, expectedNodes) {
+                expect(foundNodes.length).to.equal(expectedNodes.length);
+                expectedNodes.forEach((expectedNode, index) => {
+                    let nodeName = this.tagName(foundNodes[index]);
+                    expect(nodeName).to.equal(expectedNode);
+                });
+            },
+            tagName: function (node) {
+                return node.nodeName.toLowerCase();
+            }
+        };
+    
+    beforeEach(function() {
+        selector = elementSelector();
+        $('body').css({
+            padding: 0,
+            margin: 0
+        });
+    });
+    
+    afterEach(function() {
+        appendedTestElements.forEach(function(element) {
+            element.remove();
+        });
+        appendedTestElements = [];
+    });
+    
+    it('should return at least the root if no other elements contain the point', () => {
+        
+        helper.appendElement({
+            marginLeft: 10,
+            marginTop: 10
+        });
+        let x = 0, y = 0;
+        let foundNodes = selector.find({x, y});
+        
+        expect(helper.tagName(foundNodes[0])).to.equal('html');
+    });
+    
+    it('should return the element if it contains the point', () => {
+        
+        helper.appendElement({
+            marginLeft: 10,
+            marginTop: 10
+        });
+        let x = 15, y = 15;
+        let foundNodes = selector.find({x, y});
+        expect(helper.tagName(foundNodes[0])).to.equal('div');
+    });
+    
+    it('should return all elements that contain the point, and can usually attach listeners to',
+       () => {
+        
+        helper.appendElement({
+            marginLeft: 10,
+            marginTop: 10
+        });
+        let x = 10, y = 10;
+        let foundNodes = selector.find({x, y});
+        let expectedNodes = ['div', 'body', 'html', '#document'];
+        
+        helper.assertNodesExpectation(foundNodes, expectedNodes);
+    });
+    
+    it('should ignore elements that are overlapped by other elements', () => {
+        
+        helper.appendElement();
+        //overlap another one over it
+        let element2 = helper.appendElement({
+            position: 'absolute',
+            left: 10,
+            top: 10
+        });
+        let x = 15, y = 15;
+        let foundNodes = selector.find({x, y});
+        expect(foundNodes[0]).to.equal(element2);
+    });
+    
+    it('should ignore parent elements that do not actually contain the point', () => {
+        
+        let parent = helper.appendElement();
+        helper.appendElement({
+                parent: parent,
+                position: 'absolute',
+                left: 100,
+                top: 100
+            });
+        let x = 105, y = 105;
+        let foundNodes = selector.find({x, y});
+        // body has height of parent element, should not be in
+        let expectedNodes = ['div', '#document'];
+        
+        helper.assertNodesExpectation(foundNodes, expectedNodes);
+    });
+    
+});
