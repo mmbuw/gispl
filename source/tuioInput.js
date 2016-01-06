13e3246c8eab63a47dea91b24527bc16cbd3d8f1
Goran Košutić <kosutic.goran@gmail.com>
2016-01-06 16:23:31 +0100

    notify from tuioInput with the right information -> which node, which points currently on it

--- source/tuioInput.js
+++ source/tuioInput.js
@@ -1,11 +1,47 @@
 export default function tuioInput(params = {}) {
     let {tuioClient,
-            events} = params;
+            findNodes,
+            screenWidth = window.screen.width,
+            screenHeight = window.screen.height} = params,
+        inputApi = {},
+        listeners = new Set();
+    
+    inputApi.listen = function inputListen(callback) {
+        if (typeof callback !== 'function') {
+            throw new Error(`Attempting to register a listener that
+                                is not a function`);
+        }
+        listeners.add(callback);
+    };
+    
+    inputApi.notify = function inputNotify(...args) {
+        listeners.forEach(callback => {
+            callback(...args);
+        });
+    };
     
     // listen to tuio/websocket
     tuioClient.connect();
     tuioClient.on('refresh', () => {
-        let pointers = tuioClient.getTuioPointers();
-        events.emit(document, 'gesture');
+        let pointers = tuioClient.getTuioPointers(),
+            regions = new Map();
+        
+        pointers.forEach(pointer => {
+            let screenX = pointer.getScreenX(screenWidth),
+                screenY = pointer.getScreenY(screenHeight);
+            
+            findNodes
+                .fromPoint({screenX, screenY})
+                .forEach(node => {
+                    if (!regions.has(node)) {
+                        regions.set(node, []);
+                    }
+                    regions.get(node).push(pointer);
+                });
+        });
+        
+        inputApi.notify(regions);
     });
+    
+    return inputApi;
 }
--- tests/unit/tuioInput.js
+++ tests/unit/tuioInput.js
@@ -1,6 +1,7 @@
 import tuioInput from '../../source/tuioInput';
 import gispl from '../../source/gispl';
-import {events} from '../../source/events';
+import screenCalibration from '../../source/screenCalibration';
+import nodeSearch from '../../source/nodeSearch';
 import {WebMocket, MocketServer} from 'webmocket';
 import TuioClient from 'tuio/src/TuioClient';
 import {sendPointerBundle} from '../helpers/osc';
@@ -10,9 +11,10 @@ describe('tuioInput', () => {
     let server,
         tuioClient,
         tuioSpy,
-        eventSpy,
-        input,
-        connectionUrl = 'test-url';
+        connectionUrl = 'test-url',
+        calibration,
+        calibrationStub,
+        findNodes;
     
     beforeEach(() => {
         window.WebSocket = WebMocket;
@@ -20,33 +22,124 @@ describe('tuioInput', () => {
         
         let host = connectionUrl;        
         tuioClient = new TuioClient({host});
-        tuioSpy = sinon.spy(tuioClient, 'getTuioPointers');
-        eventSpy = sinon.spy(events, 'emit');
-        input = tuioInput({tuioClient, events});
+        
+        calibration = screenCalibration();
+        calibrationStub = sinon.stub(calibration, 'screenToViewportCoordinates')
+                            .returns({x: 0, y: 0});
+        findNodes = nodeSearch({calibration});
     });
     
     afterEach(() => {
         server.close();
-        tuioSpy.restore();
-        eventSpy.restore();
+        calibrationStub.restore();
     });
     
     it('should check for tuio pointers when pointers received', (asyncDone) => {
         
         let sessionId = 10,
-            frameId = 1,
-            alive = [sessionId],
             tuioPointer = {
-                sessionId, alive, frameId
-            };
+                sessionId
+            },
+            tuioSpy = sinon.spy(tuioClient, 'getTuioPointers');
+        
+        tuioInput({tuioClient, findNodes});
         
         setTimeout(() => {
             sendPointerBundle(server, tuioPointer);
             expect(tuioSpy.callCount).to.equal(1);
+            
             let pointers = tuioSpy.returnValues[0];
             expect(pointers[0].getSessionId()).to.equal(sessionId);
+            
+            tuioSpy.restore();
             asyncDone();
         }, 0);
+    });
+    
+    it('should allow callbacks to be registered and notified', () => {
+        let input = tuioInput({tuioClient, findNodes}),
+            spy = sinon.spy(),
+            param = 1;
+        
+        input.listen(spy);
+        
+        input.notify();
+        expect(spy.callCount).to.equal(1);
+        
+        input.notify(param);
+        expect(spy.callCount).to.equal(2);
+        expect(spy.lastCall.args[0]).to.equal(param);
+        
+        input.notify(param, param);
+        expect(spy.callCount).to.equal(3);
+        expect(spy.lastCall.args.length).to.equal(2);
+        expect(spy.lastCall.args[0]).to.equal(param);
+        expect(spy.lastCall.args[1]).to.equal(param);
+        
+        input.notify([param]);
+        expect(spy.callCount).to.equal(4);
+        expect(spy.lastCall.args[0]).to.deep.equal([param]);
+        
+        input.notify({param});
+        expect(spy.callCount).to.equal(5);
+        expect(spy.lastCall.args[0]).to.deep.equal({param});
+    });
+    
+    it('should allow only functions to be registered as callbacks', () => {
+        let input = tuioInput({tuioClient, findNodes});
+        
+        expect(function() {
+            input.listen();
+        }).to.throw();
+        
+        expect(function() {
+            input.listen({});
+        }).to.throw();
+        
+        expect(function() {
+            input.listen([]);
+        }).to.throw();
+    });
+    
+    it('should notify listeners when input received and nodes found', (asyncDone) => {
+        let sessionId = 10,
+            tuioPointer = {
+                sessionId
+            },
+            spy = sinon.spy(),
+            input = tuioInput({tuioClient, findNodes});
+        
+        input.listen(spy);
+        
+        setTimeout(() => {
+            sendPointerBundle(server, tuioPointer);
+            expect(spy.callCount).to.equal(1);
+            asyncDone();
+        });
+    });
+    
+    it('should notify listeners with the node and points information', (asyncDone) => {
+        let sessionId = 10,
+            tuioPointer = {
+                sessionId
+            },
+            spy = sinon.spy(),
+            input = tuioInput({tuioClient, findNodes});
+        
+        input.listen(spy);
+        
+        setTimeout(() => {
+            sendPointerBundle(server, tuioPointer);
+            let regions = spy.getCall(0).args[0];
+            //document is the only node found
+            //check calibration stub, retunrns 0, 0
+            expect(regions.size).to.equal(1);
+            expect(regions.has(document)).to.equal(true);
+            //only one input object ->  tuioPointer
+            expect(regions.get(document).length).to.equal(1);
+            expect(regions.get(document)[0].getSessionId()).to.equal(sessionId);
+            asyncDone();
+        });
     });
 
 });
