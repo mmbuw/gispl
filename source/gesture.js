03102cb5825b546a0fa40630b76dcaaa55cac932
Goran Košutić <kosutic.goran@gmail.com>
2016-01-07 15:01:20 +0100

    throw exceptions in a less stupid way

--- source/gesture.js
+++ source/gesture.js
@@ -13,31 +13,31 @@ export function createGesture(definition) {
 }
     
 export let gestureException = {
-    EMPTY: new Error(`Attempting to define a gesture without
-                        passing a gesture`),
-    NO_NAME: new Error('Attempting to define a gesture without name'),
-    NO_FEATURES: new Error('Attempting to define a gestures without features'),
-    DUPLICATE: new Error('Attempting to define a gesture that already exists')
+    EMPTY: `Attempting to define a gesture without
+                        passing a gesture`,
+    NO_NAME: 'Attempting to define a gesture without name',
+    NO_FEATURES: 'Attempting to define a gestures without features',
+    DUPLICATE: 'Attempting to define a gesture that already exists'
 };
     
 
 function isValidGesture(definition) {
     if (typeof definition === 'undefined' ||
             Object.keys(definition).length === 0) {
-        throw gestureException.EMPTY;
+        throw new Error(gestureException.EMPTY);
     }
     
     let {name, features} = definition;
     
     if (typeof name === 'undefined') {
-        throw gestureException.NO_NAME;
+        throw new Error(gestureException.NO_NAME);
     }
     if (typeof features === 'undefined' ||
             typeof features.length === 'undefined' ||
             features.length === 0) {
-        throw gestureException.NO_FEATURES;
+        throw new Error(gestureException.NO_FEATURES);
     }
     if (userDefinedGestures.has(name)) {
-        throw gestureException.DUPLICATE;
+        throw new Error(gestureException.DUPLICATE);
     }
 }
