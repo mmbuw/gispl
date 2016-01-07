7360f3b63020e54241415a9357c79387da5018d5
Goran Košutić <kosutic.goran@gmail.com>
2016-01-07 13:52:02 +0100

    define exceptions and throw them instead of creating new

--- source/gesture.js
+++ source/gesture.js
@@ -12,23 +12,31 @@ export function createGesture(definition) {
     return gestureApi;
 }
     
+export let gestureException = {
+    EMPTY: new Error(`Attempting to define a gesture without
+                        passing a gesture`),
+    NO_NAME: new Error('Attempting to define a gesture without name'),
+    NO_FEATURES: new Error('Attempting to define a gestures without features'),
+    DUPLICATE: new Error('Attempting to define a gesture that already exists')
+};
+    
 
 function isValidGesture(definition) {
     if (typeof definition === 'undefined') {
-        throw new Error('Attempting to define a gesture without passing a gesture');
+        throw gestureException.EMPTY;
     }
     
     let {name, features} = definition;
     
     if (typeof name === 'undefined') {
-        throw new Error('Attempting to define a gesture without name');
+        throw gestureException.NONAME;
     }
     if (typeof features === 'undefined' ||
             typeof features.length === 'undefined' ||
             features.length === 0) {
-        throw new Error('Attempting to define a gestures without features');
+        throw gestureException.NO_FEATURES;
     }
     if (userDefinedGestures.has(name)) {
-        throw new Error('Attempting to define a gesture that already exists');
+        throw gestureException.DUPLICATE;
     }
 }
