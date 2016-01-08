05df5978ecf40f3aa1e019c1211ece8c55a5f196
Goran Košutić <kosutic.goran@gmail.com>
2016-01-08 16:04:58 +0100

    basic implementation of the motion type

--- source/features/motion.js
+++ source/features/motion.js
@@ -6,5 +6,37 @@ export default function motion() {
         return type;
     };
     
+    motionApi.load = function motionLoad(inputState = []) {
+        
+        if (!inputState.length) {
+            return false;
+        }
+        
+        let matches = false,
+            result = {x: 0, y: 0};
+        
+        inputState.forEach(input => {
+            let path = input.path;
+            if (path.length > 1) {
+                let lastPoint = path[path.length-1],
+                    beforeLastPoint = path[path.length-2];
+                
+                let tempVector = {
+                    x: lastPoint.getX() - beforeLastPoint.getX(),
+                    y: lastPoint.getY() - beforeLastPoint.getY(),
+                }
+                
+                result.x += tempVector.x;
+                result.y += tempVector.y;
+            }
+        });
+        
+        // vector length
+        let resultLength = Math.sqrt(Math.pow(result.x, 2) +
+                                Math.pow(result.y, 2));
+        
+        return resultLength !== 0;
+    };
+    
     return motionApi;
 }
--- tests/unit/features/motion.js
+++ tests/unit/features/motion.js
@@ -1,11 +1,84 @@
 import {featureFactory} from '../../../source/feature';
+import TuioPointer from 'tuio/src/TuioPointer';
 
 describe('feature', () => {
     describe('motion', () => {
-        it('should construct', () => {
-            let motion = featureFactory('Motion');
-            expect(motion).to.be.an('object');
-            expect(motion.type()).to.equal('Motion');
+        
+        let motion;
+        
+        function buildPointer(params = {}) {
+            let {x:xp, y:yp} = params;
+            
+            let pointer = new TuioPointer({xp, yp});
+            
+            return {
+                moveTo: function(params) {
+                    let {x:xp, y:yp} = params;
+                    pointer.update({xp, yp});
+                    return this;
+                },
+                finished: function() {
+                    return pointer;
+                }
+            };
+        }
+        
+        beforeEach(() => {
+            motion = featureFactory('motion');
+        });
+        
+        it('should return false when passed no, empty, or invalid object', () => {
+            expect(motion.load()).to.equal(false);
+            expect(motion.load([])).to.equal(false);
+            expect(motion.load({})).to.equal(false);
+        });
+        
+        it('should recognize motion of inputs with at least two known points', () => {
+            let movingPointer = buildPointer({x: 0.5, y: 0.5})
+                                    .moveTo({x: 0.6, y: 0.6})
+                                    .finished();
+            let inputState = [movingPointer];
+            expect(motion.load(inputState)).to.equal(true);
+        });
+        
+        it('should not recognize motion of inputs with only one known point', () => {
+            let inputState = [new TuioPointer()];
+            expect(motion.load(inputState)).to.equal(false);
+        });
+        
+        it('should not recognize motion of input that moved, but stopped', () => {
+            let stoppedPointer = buildPointer({x: 0.1, y: 0.1})
+                                    .moveTo({x: 0.5, y: 0.5})
+                                    .moveTo({x: 0.1, y: 0.1})
+                                    .moveTo({x: 0.1, y: 0.1})
+                                    .finished();
+            
+            let inputState = [stoppedPointer];
+            expect(motion.load(stoppedPointer)).to.equal(false);
+        });
+        
+        it(`should recognize motion of several pointers,
+                at least one of which is moving`, () => {
+            let movingPointer = buildPointer({x: 0.5, y: 0.5})
+                                    .moveTo({x: 0.6, y: 0.6})
+                                    .finished();
+            let staticPointer = buildPointer({x: 0.1, y: 0.2})
+                                    .finished();
+            
+            let inputState = [movingPointer, staticPointer];
+            
+            expect(motion.load(inputState)).to.equal(true);
+        });
+        
+        it(`should not recognize motion of several pointers,
+                    none of which is moving`, () => {
+            let staticPointer = buildPointer({x: 0.5, y: 0.6})
+                                    .finished();
+            let staticPointer2 = buildPointer({x: 0.1, y: 0.2})
+                                    .finished();
+            let inputState = [staticPointer, staticPointer2];
+            
+            expect(motion.load(inputState)).to.equal(false);
         });
     });
 });
