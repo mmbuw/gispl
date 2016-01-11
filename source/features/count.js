4e34669df0c93afb75fde2c0996d46b166baf128
Goran Košutić <kosutic.goran@gmail.com>
2016-01-11 21:46:55 +0100

    basic handling of count feature

--- source/features/count.js
+++ source/features/count.js
@@ -1,8 +1,9 @@
 import {featureBase} from '../feature';
 
-export default function count() {
+export default function count(params) {
     let countApi = {},
-        baseApi = featureBase();
+        baseApi = featureBase(),
+        {constraints} = params;
     
     countApi.type = function countType() {
         return 'Count';
@@ -12,6 +13,12 @@ export default function count() {
         if (!baseApi.load(inputState)) {
             return false;
         }
+        let match = inputState.length >= constraints[0];
+        if (typeof constraints[1] !== 'undefined') {
+            match = match && (inputState.length <= constraints[1]);
+        }
+        
+        return match;
     };
     
     return countApi;
--- tests/unit/features/count.js
+++ tests/unit/features/count.js
@@ -4,17 +4,48 @@ import TuioPointer from 'tuio/src/TuioPointer';
 describe('feature', () => {
     describe('count', () => {
         
-        let type = 'count',
-            countFeature;
+        let type = 'count';
         
         beforeEach(() => {
-            countFeature = featureFactory({type});
         });
         
         it('should return false when passed no, empty, or invalid object', () => {
+            let countFeature = featureFactory({type});
             expect(countFeature.load()).to.equal(false);
             expect(countFeature.load([])).to.equal(false);
             expect(countFeature.load({})).to.equal(false);
+        });
+        
+        it(`should recognize the feature if the number of items in the inputState
+                has at least the number of defined constraints`, () => {
+            let twoMinInputConstraint = [2],
+                tuioPointer = null;
+            
+            let atLeastTwoInput = featureFactory({type, constraints: twoMinInputConstraint});
+            
+            expect(atLeastTwoInput.load([tuioPointer])).to.equal(false);
+            expect(atLeastTwoInput.load([tuioPointer, tuioPointer])).to.equal(true);
+            expect(atLeastTwoInput.load(
+                [tuioPointer,
+                 tuioPointer,
+                 tuioPointer,
+                 tuioPointer,
+                 tuioPointer,
+                 tuioPointer,
+                 tuioPointer,
+                 tuioPointer]
+            )).to.equal(true);
+        });
+        
+        it(`should recognize the feature if the number of items in the inputState
+                has at most the number of defined constraints`, () => {
+            let twoMaxInputConstraint = [0, 2],
+                tuioPointer = null;
+            
+            let maxTwoInput = featureFactory({type, constraints: twoMaxInputConstraint});
+            expect(maxTwoInput.load([tuioPointer])).to.equal(true);
+            expect(maxTwoInput.load([tuioPointer, tuioPointer])).to.equal(true);
+            expect(maxTwoInput.load([tuioPointer, tuioPointer, tuioPointer])).to.equal(false);
         });
     });
 });
