60357fbcc27afce49de4ae1ecf9fb7699864f1a3
Goran Košutić <kosutic.goran@gmail.com>
2016-01-15 12:57:35 +0100

    implement path feature //first try

--- source/features/path.js
+++ source/features/path.js
@@ -1,11 +1,58 @@
-import {featureFactory} from '../feature';
+import {featureBase} from '../feature';
+import {DollarRecognizer,
+            Point} from '../libs/dollar';
 
 export default function path(params) {
-    let _path = {};
+
+    isValidPathFeature(params);
+
+    let _path = {},
+        baseFeature = featureBase(params),
+        $recognizer = new DollarRecognizer(),
+        constraints = dollarPointsFrom(params.constraints);
+
+    $recognizer.AddGesture('current', constraints);
 
     _path.type = function pathType() {
         return 'Path';
     };
 
+    _path.load = function pathLoad(inputState) {
+        if (!baseFeature.validInput(inputState)) {
+            return false;
+        }
+
+        return inputState.every(input => {
+            let $points = input.path.map(point => {
+                let x = point.getScreenX(window.screen.width),
+                    y = point.getScreenY(window.screen.height);
+                return new Point(x, y);
+            });
+
+            return $recognizer.Recognize($points).Score >= 0.8;
+        });
+    };
+
     return _path;
 }
+
+function isValidPathFeature(pathFeature) {
+    if (typeof pathFeature.constraints === 'undefined'
+            || ! pathFeature.constraints.length) {
+        throw new Error(pathFeatureException.NO_CONSTRAINTS);
+    }
+}
+
+function dollarPointsFrom(constraints) {
+    return constraints.map(constraintPoint => {
+        let x = constraintPoint[0],
+            y = (1-constraintPoint[1]);
+
+        return new Point(x, y);
+    });
+}
+
+export let pathFeatureException = {
+    NO_CONSTRAINTS: `Attempting to add a path feature with no constraints;
+                        i.e. number of path points`
+};
--- tests/unit/features/path.js
+++ tests/unit/features/path.js
@@ -1,14 +1,164 @@
 import {featureFactory} from '../../../source/feature';
+import {pathFeatureException} from '../../../source/features/path';
+import TuioPointer from 'tuio/src/TuioPointer';
 
 describe('feature', () => {
     describe('path', () => {
 
-        let type = 'path';
+        function buildPointer(params = {}) {
+            let {x:xp, y:yp,
+                    typeId} = params;
+
+            let pointer = new TuioPointer({xp, yp});
+
+            //not very clean
+            if (typeof typeId !== 'undefined') {
+                pointer.typeId = typeId;
+            }
+
+            return {
+                moveTo: function(params) {
+                    let {x:xp, y:yp} = params;
+                    pointer.update({xp, yp});
+                    return this;
+                },
+                finished: function() {
+                    return pointer;
+                }
+            };
+        }
+
+        let type = 'path',
+            constraints = [[0,0]];
 
         it('should construct', () => {
-            let pathFeature = featureFactory({type});
+            let pathFeature = featureFactory({type, constraints});
             expect(pathFeature).to.be.an('object');
             expect(pathFeature.type()).to.equal('Path');
+        });
+
+        it('should return false when passed no, empty, or invalid object', () => {
+            let pathFeature = featureFactory({type, constraints});
+            expect(pathFeature.load()).to.equal(false);
+            expect(pathFeature.load([])).to.equal(false);
+            expect(pathFeature.load({})).to.equal(false);
+        });
+
+        it('should throw if missing constraints', () => {
+            expect(function() {
+                featureFactory({type});
+            }).to.throw(Error, new RegExp(pathFeatureException.NO_CONSTRAINTS));
+        });
+
+        it(`should recognize if the point in inputState roughly match
+                the points in constraints`, () => {
+            // origin is bottom left
+            let drawRectangleFromTopLeftCounterClockwise = [
+                [0, 100],
+                [0, 0],
+                [100, 0],
+                [100, 100],
+                [0, 100]
+            ],
+                rectanglePath = featureFactory({
+                    type,
+                    constraints: drawRectangleFromTopLeftCounterClockwise
+                }),
+                // tuio is with origin top left
+                rectangleMovingPointersCounterClockwise = buildPointer({
+                    x: 0.5, y: 0.5
+                }).moveTo({
+                    x: 0.5, y: 0.7
+                }).moveTo({
+                    x: 0.8, y: 0.7
+                }).moveTo({
+                    x: 0.8, y: 0.45
+                }).moveTo({
+                    x: 0.5, y: 0.5
+                }).finished();
+
+            let inputState = [rectangleMovingPointersCounterClockwise];
+
+            expect(rectanglePath.load(inputState)).to.equal(true);
+        });
+
+        it(`should recognize if the point in inputState does not match
+                the points in constraints`, () => {
+            // origin is bottom left
+            let drawRectangleFromTopLeftCounterClockwise = [
+                [0, 100],
+                [0, 0],
+                [100, 0],
+                [100, 100],
+                [0, 100]
+            ],
+                rectanglePath = featureFactory({
+                    type,
+                    constraints: drawRectangleFromTopLeftCounterClockwise
+                }),
+                // tuio is with origin top left
+                rectangleMovingPointersClockwise = buildPointer({
+                    x: 0.8, y: 0.45
+                }).moveTo({
+                    x: 0.8, y: 0.7
+                }).moveTo({
+                    x: 0.5, y: 0.7
+                }).moveTo({
+                    x: 0.5, y: 0.5
+                }).moveTo({
+                    x: 0.8, y: 0.45
+                }).finished();
+
+            let inputState = [rectangleMovingPointersClockwise];
+
+            expect(rectanglePath.load(inputState)).to.equal(false);
+        });
+
+        it(`should recognize if the points in inputState do not match
+                the points in constraints`, () => {
+            // origin is bottom left
+            let drawRectangleFromTopLeftCounterClockwise = [
+                [0, 100],
+                [0, 0],
+                [100, 0],
+                [100, 100],
+                [0, 100]
+            ],
+                rectanglePath = featureFactory({
+                    type,
+                    constraints: drawRectangleFromTopLeftCounterClockwise
+                }),
+                //should match
+                rectangleMovingPointersCounterClockwise = buildPointer({
+                    x: 0.5, y: 0.5
+                }).moveTo({
+                    x: 0.5, y: 0.7
+                }).moveTo({
+                    x: 0.8, y: 0.7
+                }).moveTo({
+                    x: 0.8, y: 0.45
+                }).moveTo({
+                    x: 0.5, y: 0.5
+                }).finished(),
+                // should not match
+                rectangleMovingPointersClockwise = buildPointer({
+                    x: 0.8, y: 0.45
+                }).moveTo({
+                    x: 0.8, y: 0.7
+                }).moveTo({
+                    x: 0.5, y: 0.7
+                }).moveTo({
+                    x: 0.5, y: 0.5
+                }).moveTo({
+                    x: 0.8, y: 0.45
+                }).finished();
+
+            let inputState = [
+                rectangleMovingPointersCounterClockwise,
+                rectangleMovingPointersClockwise
+            ];
+
+            expect(rectanglePath.load(inputState)).to.equal(false);
         });
     });
 });
