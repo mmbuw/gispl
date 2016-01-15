838ab4d92c3723e3095a5dcb60bba6234ff46f8f
Goran Košutić <kosutic.goran@gmail.com>
2016-01-15 10:05:08 +0100

    small test refactor

--- tests/integration/dollar.js
+++ tests/integration/dollar.js
@@ -3,6 +3,8 @@ import {DollarRecognizer,
 
 describe('dollar recognizer', () => {
 
+    let $r = new DollarRecognizer();
+
     it('should construct', () => {
         expect(new DollarRecognizer()).to.be.an('object');
     });
@@ -23,8 +25,7 @@ describe('dollar recognizer', () => {
         rectangleDrawnFromTopLeft.push(new Point(60,50));
         rectangleDrawnFromTopLeft.push(new Point(55,50));
 
-        let $r = new DollarRecognizer(),
-            result = $r.Recognize(rectangleDrawnFromTopLeft, true);
+        let result = $r.Recognize(rectangleDrawnFromTopLeft, true);
         expect(result.Name).to.equal('rectangle');
     });
 
@@ -37,8 +38,7 @@ describe('dollar recognizer', () => {
         triangleDrawnFromTopToLeft.push(new Point(200, 200));
         triangleDrawnFromTopToLeft.push(new Point(90, 90));
 
-        let $r = new DollarRecognizer(),
-            result = $r.Recognize(triangleDrawnFromTopToLeft, true);
+        let result = $r.Recognize(triangleDrawnFromTopToLeft, true);
 
         expect(result.Name).to.equal('triangle');
     });
