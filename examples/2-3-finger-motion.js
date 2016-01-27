fd10715c4f180fb66d91aa80ba580b5e3d9c61f9
Goran Košutić <kosutic.goran@gmail.com>
2016-01-27 11:06:36 +0100

    rename demo to show what it actually does

---
+++ examples/2-3-finger-motion.js
@@ -0,0 +1,67 @@
+import gispl from '../source/gispl';
+import $ from 'jquery';
+
+$(document).ready(() => {
+    let anyMotion = 'any-motion';
+
+    gispl.addGesture({
+        name: anyMotion,
+        features: [
+            {type: "Motion"},
+            {type: "Count", constraints: [2,3]}
+        ]
+    });
+
+    let images$ = $('img'),
+        offsets = $.map($.makeArray(images$), (element, index) => {
+            let offset = $(element).offset(),
+                left = offset.left,
+                top = offset.top;
+
+            return {left, top};
+        });
+
+    images$ = images$.each((index, element) => {
+        let element$ = $(element),
+            offset = offsets[index];
+
+        element$.css({
+            position: 'absolute',
+            top: offset.top,
+            left: offset.left
+        });
+    });
+
+    let zIndex = 1,
+        currentIdentifiers = [],
+        inImagePositions = {},
+        inImagePositionX, inImagePositionY;
+
+    gispl(images$).on(anyMotion, function(inputState) {
+        let this$ = $(this),
+            input = inputState[0],
+            {identifier,
+                clientX,
+                clientY} = input,
+            unknownIdentifier = currentIdentifiers.indexOf(identifier) === -1;
+        
+        if (unknownIdentifier) {
+            let nodePosition = this.getBoundingClientRect();
+            inImagePositions[identifier] = {};
+            inImagePositions[identifier].x = clientX - nodePosition.left;
+            inImagePositions[identifier].y = clientY - nodePosition.top;
+            currentIdentifiers.push(identifier);
+        }
+
+        let left = clientX - inImagePositions[identifier].x,
+            top = clientY - inImagePositions[identifier].y;
+
+        this$.css({zIndex, left, top});
+
+        zIndex += 1;
+    });
+
+    gispl.initTuio({
+        host: 'ws://localhost:8080'
+    });
+});
