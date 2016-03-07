fd273ae7bc2993d917fe240fe0303070912fa3b2
Goran Košutić <kosutic.goran@gmail.com>
2016-03-07 11:03:03 +0100

    update rotation demo to work with objects

--- examples/rotation.js
+++ examples/rotation.js
@@ -2,19 +2,42 @@ import gispl from '../source/gispl';
 import $ from 'jquery';
 
 $(document).ready(() => {
-    let rotate = 'rotate';
+    let fingerRotation = 'finger-rotation',
+        objectRotation = 'object-rotation';
     
     gispl.addGesture({
-        name: rotate,
+        name: fingerRotation,
         features: [
             {type: 'Count', constraints: [2,2]},
-            {type: 'Rotation', constraints:[Math.PI / 4]},
+            {type: 'Rotation'},
         ]
     });
     
-    gispl('img').on(rotate, function(event) {
+    gispl.addGesture({
+        name: objectRotation,
+        features: [
+            {type: 'Count', constraints: [1,1]},
+            {type: 'Rotation'},
+        ]
+    });
+    
+    gispl('img').on(fingerRotation, function(event) {
         let image$ = $(this),
             rotation = event.featureValues.rotation.touches;
+            
+        let degrees = rotation / Math.PI * 180;
+            
+        image$.css({
+            transform: `rotate(${degrees}deg)`
+        });
+    });
+    
+    gispl('img').on(objectRotation, function(event) {
+        let image$ = $(this),
+            objects = event.featureValues.rotation.objects,
+            objectsKeys = Object.keys(objects),
+            key = objectsKeys[objectsKeys.length-1],
+            rotation = objects[key];
             
         let degrees = rotation / Math.PI * 180;
             
