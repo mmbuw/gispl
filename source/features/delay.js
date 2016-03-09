bacf3e0fd7b76041160bfbf8885301be3c2f934b
Goran Košutić <kosutic.goran@gmail.com>
2016-03-09 10:53:37 +0100

    basic implementation of delay, #15

--- source/features/delay.js
+++ source/features/delay.js
@@ -1,7 +1,27 @@
 export default function delay(params) {
+    
+    let {constraints} = params;
+    
     return {
         type() {
             return 'Delay';
+        },
+        load(inputState) {
+            let {inputObjects} = inputState;
+            
+            return inputObjects.every(inputObject => {
+                let currentTime = Date.now();
+                
+                let match = (currentTime - inputObject.startingTime) >=
+                            (constraints[0] * 1000);
+                            
+                if (typeof constraints[1] !== 'undefined') {
+                    match = match && ((currentTime - inputObject.startingTime) <=
+                            (constraints[1] * 1000));
+                }
+                
+                return match;
+            });
         }
     };
 }
--- tests/unit/features/delay.js
+++ tests/unit/features/delay.js
@@ -1,15 +1,88 @@
 import {featureFactory} from '../../../source/feature';
+import {buildInputFromPointer} from '../../helpers/pointer';
 
 describe('feature', () => {
     
     describe('delay', () => {
         
+        let type = 'delay',
+            clock;
+            
+        beforeEach(() => {
+            clock = sinon.useFakeTimers(Date.now());
+        });
+        
+        afterEach(() => {
+            clock.restore();
+        });
+        
         it('should construct', () => {
-            let type = 'delay',
-                delayFeature = featureFactory({type});
+            let delayFeature = featureFactory({type});
             
             expect(delayFeature).to.be.an('object');
             expect(delayFeature.type()).to.equal('Delay');
+        });
+        
+        it(`should recognize a feature if the lower limit constraint matches`, () => {
+            let timeIncrementInMs = 1000,
+                minOneSecond = timeIncrementInMs / 1000,
+                delayOfMinOneSecond = featureFactory({
+                   type, constraints: [minOneSecond]
+                });
+            
+            let inputObject = buildInputFromPointer();
+            
+            clock.tick(timeIncrementInMs);
+            
+            let inputObjects = [inputObject.finished()];
+            expect(delayOfMinOneSecond.load({inputObjects})).to.equal(true);
+        });
+        
+        it(`should not recognize a feature if the lower limit constraint
+                does not match`, () => {
+            let timeIncrementInMs = 999,
+                minOneSecond = 1,
+                delayOfMinOneSecond = featureFactory({
+                    type, constraints: [minOneSecond]
+                });
+            
+            let inputObject = buildInputFromPointer();
+            
+            clock.tick(timeIncrementInMs);
+            
+            let inputObjects = [inputObject.finished()];
+            expect(delayOfMinOneSecond.load({inputObjects})).to.equal(false);
+        });
+        
+        it(`should recognize a feature if the upper limit constraint matches`, () => {
+            let timeIncrementInMs = 1000,
+                maxOneSecond = timeIncrementInMs / 1000,
+                delayOfMaxOneSecond = featureFactory({
+                   type, constraints: [0, maxOneSecond]
+                });
+            
+            let inputObject = buildInputFromPointer();
+            
+            clock.tick(timeIncrementInMs);
+            
+            let inputObjects = [inputObject.finished()];
+            expect(delayOfMaxOneSecond.load({inputObjects})).to.equal(true);
+        });
+        
+        it(`should not recognize a feature if the lower limit constraint
+                does not match`, () => {
+            let timeIncrementInMs = 1001,
+                maxOneSecond = 1,
+                delayOfMaxOneSecond = featureFactory({
+                    type, constraints: [0, maxOneSecond]
+                });
+            
+            let inputObject = buildInputFromPointer();
+            
+            clock.tick(timeIncrementInMs);
+            
+            let inputObjects = [inputObject.finished()];
+            expect(delayOfMaxOneSecond.load({inputObjects})).to.equal(false);
         });
     });
 });
