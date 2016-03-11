9e5dfdfce28a586430ef3003d3e0866eed6dc8e7
Goran Košutić <kosutic.goran@gmail.com>
2016-03-11 11:13:09 +0100

    finish implementing objectGroup, close #38

--- source/features/objectGroup.js
+++ source/features/objectGroup.js
@@ -1,10 +1,14 @@
-import {calculateCentroidFrom,
+import {featureBase,
+        calculateCentroidFrom,
         pointToPointDistance,
         lowerUpperLimit} from '../feature';
 
 export function objectgroup(params) {
     
+    isValidObjectGroupFeature(params);
+    
     let {constraints} = params,
+        baseFeature = featureBase(params),
         limit = lowerUpperLimit(constraints),
         radius = constraints[2];
     
@@ -13,16 +17,52 @@ export function objectgroup(params) {
             return 'ObjectGroup';
         },
         load(inputState) {
-            let {inputObjects} = inputState,
-                count = inputObjects.length;
+            let inputObjects = baseFeature.inputObjectsFrom(inputState)
+                                            .filter(baseFeature.checkAgainstDefinition),
+                count = inputObjects.length,
+                match = false;
             
-            let centroid = calculateCentroidFrom(inputObjects),
-                distance = pointToPointDistance(centroid, inputObjects[0], true);
-                
-            let match = Math.floor(distance) <= radius;
-            
-            return match && count >= limit.lower &&
+            if (inputObjects.length > 1) {
+                let centroid = calculateCentroidFrom(inputObjects),
+                    distance = pointToPointDistance(centroid, inputObjects[0], true);
+                    
+                match = Math.floor(distance) <= radius &&
+                        count >= limit.lower &&
                         count <= limit.upper;
-        }
+            
+                if (match) {
+                    baseFeature.setMatchedValue(distance);
+                }   
+            }
+            
+            return match;
+        },
+        setValueToObject: baseFeature.setValueToObject
     };
 }
+
+function isValidObjectGroupFeature(params) {
+    let {constraints} = params;
+    
+    if (typeof constraints === 'undefined') {
+        throw new Error(objectGroupException.NO_CONSTRAINTS);
+    }
+    if (!Array.isArray(constraints) ||
+            constraints.length !== 3) {
+        throw new Error(`${objectGroupException.INVALID_CONSTRAINTS};
+                            received ${constraints}`);
+    }
+    constraints.forEach(value => {
+        if (typeof value !== 'number') {
+            throw new Error(`${objectGroupException.INVALID_CONSTRAINTS};
+                received: ${typeof constraints[0]}, ${typeof constraints[1]}`);
+        } 
+    });
+}
+
+export const objectGroupException = Object.freeze({
+    NO_CONSTRAINTS: `Attempting to add an objectGroup feature with no constraints;
+                        i.e. lower and upper input limit, and radius`,
+    INVALID_CONSTRAINTS: `Attempting to add an objectGroup feature with invalid constraints;
+                            has to contain lower and upper input limit, and radius`
+});
--- tests/unit/features/objectGroup.js
+++ tests/unit/features/objectGroup.js
@@ -1,8 +1,11 @@
 import {featureFactory} from '../../../source/feature';
 import {buildInputFromPointer} from '../../helpers/pointer';
+import {objectGroupException} from '../../../source/features/objectGroup';
 
 describe('feature', () => {
     describe('objectGroup', () => {
+        
+        let type = 'objectGroup';
         
         it('should recognize the feature if input within constraints', () => {
             // centroid is 0.5, 0.5
@@ -14,8 +17,7 @@ describe('feature', () => {
                 // both points will be in
                 radius = Math.abs((firstInput.screenX - secondInput.screenX) / 2);
                 
-            let type = 'objectGroup',
-                count = inputObjects.length,
+            let count = inputObjects.length,
                 constraints = [count, count, radius],
                 objectGroupFeature = featureFactory({type, constraints});
             
@@ -33,8 +35,7 @@ describe('feature', () => {
                 // both points will be out
                 radius = Math.abs((firstInput.screenX - secondInput.screenX) / 2) - 1;
                 
-            let type = 'objectGroup',
-                count = inputObjects.length,
+            let count = inputObjects.length,
                 constraints = [count, count, radius],
                 objectGroupFeature = featureFactory({type, constraints});
             
@@ -52,8 +53,7 @@ describe('feature', () => {
                 // both points will be out
                 radius = Math.abs((firstInput.screenX - secondInput.screenX) / 2) + 100;
                 
-            let type = 'objectGroup',
-                count = inputObjects.length + 1,
+            let count = inputObjects.length + 1,
                 constraints = [count, count, radius],
                 objectGroupFeature = featureFactory({type, constraints});
             
@@ -71,12 +71,107 @@ describe('feature', () => {
                 // both points will be out
                 radius = Math.abs((firstInput.screenX - secondInput.screenX) / 2) + 100;
                 
-            let type = 'objectGroup',
-                count = inputObjects.length - 1,
+            let count = inputObjects.length - 1,
                 constraints = [count, count, radius],
                 objectGroupFeature = featureFactory({type, constraints});
             
             expect(objectGroupFeature.load({inputObjects})).to.equal(false);
+        });
+        
+        it('should not recognize the feature when using just one point', () => {
+            let firstInput = buildInputFromPointer({x: 0.4, y: 0.4})
+                                .finished(),
+                inputObjects = [firstInput],
+                radius = 1000;
+                
+            let count = inputObjects.length,
+                constraints = [count, count, radius],
+                objectGroupFeature = featureFactory({type, constraints});
+            
+            expect(objectGroupFeature.load({inputObjects})).to.equal(false);
+        });
+
+        it(`should not recognize the feature if the input does not match
+                the defined filter`, () => {
+            let tuioRightThumbFinger = 0b10000,
+                tuioRightIndexFinger = 1,
+                tuioRightMiddleFinger = 2,
+                firstInvalidInput = buildInputFromPointer({
+                        x: 0.4, y: 0.4, typeId: tuioRightIndexFinger
+                    }).finished(),
+                secondInvalidInput = buildInputFromPointer({
+                        x: 0.6, y: 0.4, typeId: tuioRightMiddleFinger
+                    }).finished(),
+                inputObjects = [firstInvalidInput,secondInvalidInput],
+                radius = 10000;
+           
+            let count = inputObjects.length,
+                constraints = [count, count, radius],
+                filteredObjectGroup = featureFactory({
+                    type,
+                    filters: tuioRightThumbFinger,
+                    constraints
+                });
+
+            expect(
+                filteredObjectGroup.load({inputObjects})
+            ).to.equal(false);
+        });
+        
+        it('should be able to set its last known value in the feature values object', () => {
+            // centroid is 0.5, 0.5
+            let firstInput = buildInputFromPointer({x: 0.4, y: 0.4})
+                                .finished(),
+                secondInput = buildInputFromPointer({x: 0.6, y: 0.4})
+                                .finished(),
+                inputObjects = [firstInput, secondInput],
+                // both points will be in
+                radius = Math.abs((firstInput.screenX - secondInput.screenX) / 2);
+                
+            let count = inputObjects.length,
+                constraints = [count, count, radius],
+                objectGroupFeature = featureFactory({type, constraints});
+            
+            expect(objectGroupFeature.load({inputObjects})).to.equal(true);
+            objectGroupFeature.load({inputObjects});
+            
+            let featureValues = {},
+                expectedValue = radius;
+            objectGroupFeature.setValueToObject(featureValues);
+            
+            expect(featureValues.objectgroup).to.be.above(expectedValue - 0.01);
+            expect(featureValues.objectgroup).to.be.below(expectedValue + 0.01);
+        });
+        
+        it('should throw when defining the feature without valid constraints', () => {
+            expect(() => {
+                featureFactory({type});
+            }).to.throw(Error, new RegExp(objectGroupException.NO_CONSTRAINTS));
+            
+            expect(() => {
+                let constraints = {};
+                featureFactory({type, constraints});
+            }).to.throw(Error, new RegExp(objectGroupException.INVALID_CONSTRAINTS));
+            
+            expect(() => {
+                let constraints = [];
+                featureFactory({type, constraints});
+            }).to.throw(Error, new RegExp(objectGroupException.INVALID_CONSTRAINTS));
+            
+            expect(() => {
+                let constraints = [0];
+                featureFactory({type, constraints});
+            }).to.throw(Error, new RegExp(objectGroupException.INVALID_CONSTRAINTS));
+            
+            expect(() => {
+                let constraints = [0, 1];
+                featureFactory({type, constraints});
+            }).to.throw(Error, new RegExp(objectGroupException.INVALID_CONSTRAINTS));
+            
+            expect(() => {
+                let constraints = [0, 1, '2'];
+                featureFactory({type, constraints});
+            }).to.throw(Error, new RegExp(objectGroupException.INVALID_CONSTRAINTS));
         });
     });
 });
