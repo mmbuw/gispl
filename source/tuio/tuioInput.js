c78066ac5a13670731d406e2878b28bc4169052a
Goran Košutić <kosutic.goran@gmail.com>
2016-03-17 11:50:38 +0100

    refactor and comment

--- source/tuio/tuioInput.js
+++ source/tuio/tuioInput.js
@@ -11,13 +11,13 @@ export default function tuioInput(params = {}) {
         nodesWithInput = new Map();
         
     function findNodesFromInputObject(inputObject) {
-        let node = findNode.fromPoint(inputObject);
-        if (node) {
-            if (!nodesWithInput.has(node)) {
-                nodesWithInput.set(node, []);
+        let foundNode = findNode.fromPoint(inputObject);
+        if (foundNode) {
+            if (!nodesWithInput.has(foundNode)) {
+                nodesWithInput.set(foundNode, []);
             }
-            nodesWithInput.get(node).push(inputObject);
-            tuioInputHistory.add(node, inputObject);   
+            nodesWithInput.get(foundNode).push(inputObject);
+            tuioInputHistory.add(foundNode, inputObject);   
         }
     }
 
@@ -99,12 +99,26 @@ export default function tuioInput(params = {}) {
         }
     };
 }
-
+// a helper object for tuioInput
+// it creates, keeps and updates instances of inputObjects
+// that correspond to tuioComponents like cursors or pointers
+// e.g. tuioComponent.getSessionId() === inputObject.identifier
+// normally used by tuioInput like
+// * passes tuioComponents to nodeInputHistory.store()
+// * nodeInputHistory returns a list of matching inputObjects
+// * tuioInput iterates the list, locates dom nodes that the inputObject is on
+// * calls nodeInputHistory.add(node, inputObject) to place it in history
 function nodeInputHistory(params = {}) {
+        // list of stored inputObject instances used by all nodes
     let storedObjects = [],
+        // a map of node => [inputObjects]
+        // all inputObjects that were in contact with the node at one point
         nodeHistory = new WeakMap(),
+        // limit for all stored objects
         {limit = 10} = params;
     
+    // find matching inputObject for a tuioComponent
+    // matches per id
     function findIndexOf(tuioComponent) {
         let indexOfComponent = -1;  
         storedObjects.forEach((object, index) => {
@@ -114,7 +128,11 @@ function nodeInputHistory(params = {}) {
         });
         return indexOfComponent;
     }
-    
+    // removes an inputObject from node => [inputObjects]
+    // if the instance does not exist in storedObjects
+    // this is done because once the limit for storedObjects is reached
+    // it will remove the first element from the list
+    // but it is still in the list for an individual node history
     function removeDroppedInputObjectsFrom(historyForNode) {
         historyForNode.forEach((inputObject, currentIndex) => {
             let notStored = storedObjects.indexOf(inputObject) === -1;
@@ -122,6 +140,32 @@ function nodeInputHistory(params = {}) {
                 historyForNode.splice(currentIndex, 1);
             }
         });
+    }
+    // takes a tuioComponent and either
+    // finds and updates a matching inputObject
+    // or creates a new inputObject
+    // this is based on sessionId
+    function convertToInputObject(tuioComponent, calibration) {
+        let indexOfComponent = findIndexOf(tuioComponent),
+            newComponent = indexOfComponent === -1,
+            inputObject;
+        
+        if (newComponent) {
+            let storeLimitReached = storedObjects.length === limit;
+            inputObject = inputObjectFromTuio({
+                tuioComponent,
+                calibration
+            });
+            if (storeLimitReached) {
+                storedObjects.shift();
+            }
+            storedObjects.push(inputObject);
+        }
+        else {
+            inputObject = storedObjects[indexOfComponent];
+            tuioObjectUpdate({inputObject, tuioComponent, calibration});
+        }
+        return inputObject;
     }
         
     return {
@@ -140,29 +184,11 @@ function nodeInputHistory(params = {}) {
             }
             return this;
         },
+        // returns an array of inputObjects that correspond to tuioComponents
         store(tuioComponents, calibration) {
-            return tuioComponents.map(tuioComponent => {
-                let indexOfComponent = findIndexOf(tuioComponent),
-                    newComponent = indexOfComponent === -1,
-                    inputObject;
-                
-                if (newComponent) {
-                    let storeLimitReached = storedObjects.length === limit;
-                    inputObject = inputObjectFromTuio({
-                        tuioComponent,
-                        calibration
-                    });
-                    if (storeLimitReached) {
-                        storedObjects.shift();
-                    }
-                    storedObjects.push(inputObject);
-                }
-                else {
-                    inputObject = storedObjects[indexOfComponent];
-                    tuioObjectUpdate({inputObject, tuioComponent, calibration});
-                }
-                return inputObject;
-            });
+            return tuioComponents.map(
+                tuioComponent => convertToInputObject(tuioComponent, calibration) 
+            );
         }
     };
 }
