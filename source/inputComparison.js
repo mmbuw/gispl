51f74a24bd1bbad0d560ac30bccbadaf5a522822
Goran Košutić <kosutic.goran@gmail.com>
2016-03-29 11:46:13 +0200

    reduce object creation when comparing current and previous input

--- source/inputComparison.js
+++ source/inputComparison.js
@@ -3,32 +3,50 @@
 // but also [3,2,1]
 export function compareInput(first, second) {
     let equalLength = first.length === second.length,
-        secondContainsAllOfFirst = first.every(
-            item => second.indexOf(item) !== -1
-        );
+        secondContainsAllOfFirst = true;
+        
+    for (let i = 0; i < first.length; i += 1) {
+        let item = first[i];
+        if (second.indexOf(item) === -1) {
+            secondContainsAllOfFirst = false;
+            break;
+        }
+    }
 
     return equalLength && secondContainsAllOfFirst;
-}
-// returns an array of identifiers [1,2,...]
-// from an array of inputObjects
-// [{identifier: 1,...}, {identifier: 2,...},...}
-function extractIdentifiersFrom(inputObjects = []) {
-    return inputObjects
-        .filter(inputObject => !!inputObject)
-        .map(inputObject => inputObject.identifier);
 }
 
 export function inputComparison() {
 
-    let currentInputIds,
+    let currentInputIds = [],
         matchedInputIds = [],
         previousInputIds = [],
         currentInputPreviouslyMatched,
         currentInputPreviouslyUsed;
+        
+    // returns an array of identifiers [1,2,...]
+    // from an array of inputObjects
+    // [{identifier: 1,...}, {identifier: 2,...},...}
+    function extractIdentifiersFrom(inputObjects = []) {
+        currentInputIds.length = 0;
+        for (let i = 0; i < inputObjects.length; i += 1) {
+            let inputObject = inputObjects[i];
+            if (inputObject) {
+                currentInputIds.push(inputObject.identifier);
+            }
+        }
+    }
+    
+    function setSecondAsFirst(first, second) {
+        first.length = 0;
+        for (let i = 0; i < second.length; i += 1) {
+            first.push(second[i]);
+        }
+    }
 
     return {
         use(inputObjects) {
-            currentInputIds = extractIdentifiersFrom(inputObjects);
+            extractIdentifiersFrom(inputObjects);
             //
             currentInputPreviouslyMatched = compareInput(currentInputIds,
                                                             matchedInputIds);
@@ -36,10 +54,10 @@ export function inputComparison() {
             currentInputPreviouslyUsed = compareInput(currentInputIds,
                                                         previousInputIds);
             // save for the next time the .load method is called
-            previousInputIds = currentInputIds;
+            setSecondAsFirst(previousInputIds, currentInputIds);
         },
         matched() {
-            matchedInputIds = currentInputIds;
+            setSecondAsFirst(matchedInputIds, currentInputIds);
         },
         previouslyMatched() {
             return currentInputPreviouslyMatched;
