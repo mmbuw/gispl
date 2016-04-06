d6168d8dc8aa8030be57913bc65fe138923e1b00
Goran Košutić <kosutic.goran@gmail.com>
2016-04-06 10:27:21 +0200

    clear screen values from demo on object removal

--- examples/partBench.js
+++ examples/partBench.js
@@ -5,6 +5,9 @@ $(document).ready(() => {
     let stamp = 'stamp',
         washingMachine = 'washing-machine',
         washingTokenId = 3,
+        stampTokenId = 0,
+        distanceTokenId1 = 1,
+        distanceTokenId2 = 2,
         distanceAsGroup = 'distance-group';
     
     gispl.addGesture({
@@ -12,7 +15,9 @@ $(document).ready(() => {
         flags: 'oneshot',
         features: [
             {type: 'Count', constraints: [1, 1]},
-            {type: 'ObjectId', constraints: [0, 0]}
+            {type: 'ObjectId', constraints: [
+                stampTokenId, stampTokenId
+            ]}
         ]
     });
     
@@ -30,7 +35,9 @@ $(document).ready(() => {
         name: distanceAsGroup,
         features: [
             {type: 'ObjectGroup', constraints: [2, 2, window.screen.width]},
-            {type: 'ObjectId', constraints: [1, 2]}
+            {type: 'ObjectId', constraints: [
+                distanceTokenId1, distanceTokenId2
+            ]}
         ]
     });
     
@@ -44,23 +51,43 @@ $(document).ready(() => {
     canvas.width = document.body.clientWidth;
     canvas.height = document.body.clientHeight;
     
-    gispl(canvas).on(stamp, function(event) {
-        let touchPosition = event.input[0];
-        stampPosition.pageX = touchPosition.pageX;
-        stampPosition.pageY = touchPosition.pageY;
-        requestDraw();
-    });
-    
-    gispl(canvas).on(washingMachine, function(event) {
-        washingRotation = event.featureValues.rotation.objects[washingTokenId] * 180 / Math.PI;
-        washingRotation = washingRotation % 360;
-        requestDraw();
-    });
-    
-    gispl(canvas).on(distanceAsGroup, function(event) {
-        distanceRadius = event.featureValues.objectgroup.radius;
-        requestDraw();
-    });
+    gispl(canvas)
+        .on(stamp, function onStamp(event) {
+            let touchPosition = event.input[0];
+            stampPosition.pageX = touchPosition.pageX;
+            stampPosition.pageY = touchPosition.pageY;
+            requestDraw();
+        })
+        .on(washingMachine, function onWashingMachine(event) {
+            washingRotation = event.featureValues.rotation.objects[washingTokenId] * 180 / Math.PI;
+            washingRotation = washingRotation % 360;
+            requestDraw();
+        })
+        .on(distanceAsGroup, function onDistanceAsGroup(event) {
+            distanceRadius = event.featureValues.objectgroup.radius;
+            requestDraw();
+        })
+        .on('inputend', function onInputEnd(event) {
+            let {input} = event,
+                washingTokensOnScreen = 0,
+                distanceTokensOnScreen = 0;
+            for (let i = 0; i < input.length; i += 1) {
+                let componentId = input[i].componentId;
+                if (componentId === washingTokenId) {
+                    washingTokensOnScreen += 1;
+                }
+                else if (componentId === distanceTokenId1 ||
+                            componentId === distanceTokenId2) {
+                    distanceTokensOnScreen += 1;
+                }
+            }
+            if (washingTokensOnScreen === 0) {
+                washingRotation = 0;
+            }
+            if (distanceTokensOnScreen !== 2) {
+                distanceRadius = 0;
+            }
+        });
     
     function requestDraw() {
         if (!drawing) {
